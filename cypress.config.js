66fa7dcdff11b61360c9ca49182665561d47ebd4
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-09-02 13:52:24 -0300

    build: Implement Basic Configuration for the Project

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportWidth: 1200,
+  viewportHeight: 660,
+  e2e: {
+    baseUrl: "http://localhost:5173",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    chromeWebSecurity: false
+  },
+});
